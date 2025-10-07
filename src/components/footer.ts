a423f33d10d4e2db0c4eb4d27e924e82510cd33c
leightongrant <oneal.grant@outlook.com>
2025-10-07 21:42:34 +0100

    feat(footer): add footer component with navigation buttons

---
+++ src/components/footer.ts
@@ -0,0 +1,24 @@
+const footer = () => {
+	return `
+        <div class="flex-grow-1 text-center">
+            <div>
+                <button>Home Icon</button>
+                <span class="d-block">Home</span>
+            </div>
+        </div>
+        <div class="flex-grow-1 text-center">
+            <div>
+                <button>Date Picker</button>
+                <span class="d-block">Date</span>
+            </div>
+        </div>
+        <div class="flex-grow-1 text-center">
+            <div>
+                <button>Settings Icon</button>
+                <span class="d-block">Settings</span>
+            </div>
+        </div>
+        `
+}
+
+export default footer
