9ac0ac33c0211fda9e4a954a99a068a3d73a4772
leightongrant <oneal.grant@outlook.com>
2025-10-07 21:43:27 +0100

    style(layout): improve structure and styling of layout component

--- src/components/layout.ts
+++ src/components/layout.ts
@@ -1,8 +1,26 @@
 const layout = (header: string, main: string, footer: string): string => {
 	return `
-    <header>${header}<header>
-    <main>${main}</main>
-    <footer>${footer}</footer>
+    <header>
+        <div class="container-fluid">
+            <div class="header-content p-5">
+                ${header}
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container-fluid">
+            <div class="main-content px-5 py-1 d-grid gap-2">
+                ${main}
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="container-fluid">
+            <div class="footer-content d-flex gap-5 flex-fill">
+                ${footer}
+            </div>
+        </div>
+    </footer>
 
 `
 }
