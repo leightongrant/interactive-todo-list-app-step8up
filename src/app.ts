35af3511ddd12442dbc04dcfaeb3ebb6288fde95
Leighton Grant <oneal.grant@outlook.com>
2025-10-12 01:55:15 +0100

    feat: Implement theme selection in settings
    This introduces a new feature allowing users to customize the application's color theme via a new settings modal.
    
    - Adds a settings modal with a form containing radio buttons for 'Default', 'Crimson', and 'Hot Pink' theme options.
    - The selected theme is saved to localStorage and applied on page load, ensuring persistence between sessions.
    - Implements new CSS classes and variables to dynamically update the site's primary colors and shadows based on the chosen theme.

--- src/app.ts
+++ src/app.ts
@@ -13,12 +13,24 @@ const calendarButton = document.querySelector('div[name=date]') as HTMLButtonEle
 const homeButton = document.querySelector('div[name=home]') as HTMLButtonElement
 const settingsButton = document.querySelector('div[name=settings]') as HTMLButtonElement
 const dateModal = document.querySelector('#date-modal') as HTMLDialogElement
-
+const bodyElement = document.querySelector('body') as HTMLBodyElement
 const cancelDateDialog = document.querySelector('#date-cancel') as HTMLButtonElement
 const confirmDateDialog = document.querySelector('#date-confirm') as HTMLButtonElement
 const settingsModal = document.querySelector('#settings-modal') as HTMLDialogElement
 const cancelSettingsDialog = document.querySelector('#settings-cancel') as HTMLButtonElement
 const confirmSettingsDialog = document.querySelector('#settings-confirm') as HTMLButtonElement
+const settingsForm = document.querySelector('.settings-form') as HTMLFormElement
+
+const themeName = localStorage.getItem('theme')
+if (typeof themeName === 'string') {
+	if (themeName === 'default') {
+		bodyElement.removeAttribute('class')
+	} else {
+		bodyElement.classList.add(themeName)
+	}
+} else {
+	bodyElement.removeAttribute('class')
+}
 
 let timestamp = Date.now()
 const currentDay = localStorage.getItem('day')
@@ -28,18 +40,6 @@ if (typeof currentDay === 'string') {
 }
 
 renderMainContent(mainContent, renderTasks(timestamp))
-
-const dateInput = document.querySelector('input[name=date]') as HTMLInputElement
-cancelDateDialog.addEventListener('click', () => dateModal.close())
-cancelSettingsDialog.addEventListener('click', () => settingsModal.close())
-
-confirmDateDialog.addEventListener('click', () => {
-	const [year, month, day] = dateInput.value.split('-')
-	const selectedTimestamp = new Date(parseInt(year), parseInt(month) - 1, parseInt(day)).getTime()
-	localStorage.setItem('day', selectedTimestamp.toString())
-	renderMainContent(mainContent, renderTasks(selectedTimestamp))
-	dateModal.close()
-})
 
 calendarButton.addEventListener('click', () => {
 	dateModal.showModal()
@@ -53,4 +53,26 @@ homeButton.addEventListener('click', () => {
 
 settingsButton.addEventListener('click', () => {
 	settingsModal.showModal()
+})
+
+const dateInput = document.querySelector('input[name=date]') as HTMLInputElement
+cancelDateDialog.addEventListener('click', () => dateModal.close())
+cancelSettingsDialog.addEventListener('click', () => settingsModal.close())
+
+confirmDateDialog.addEventListener('click', () => {
+	const [year, month, day] = dateInput.value.split('-')
+	const selectedTimestamp = new Date(parseInt(year), parseInt(month) - 1, parseInt(day)).getTime()
+	localStorage.setItem('day', selectedTimestamp.toString())
+	renderMainContent(mainContent, renderTasks(selectedTimestamp))
+	dateModal.close()
+})
+
+confirmSettingsDialog.addEventListener('click', () => {
+	settingsForm.requestSubmit()
+})
+settingsForm.addEventListener('submit', (e: Event) => {
+	e.preventDefault()
+	const target = e.target as HTMLFormElement
+	localStorage.setItem('theme', target.theme.value)
+	location.replace('/')
 })
--- src/components/settingsModal.ts
+++ src/components/settingsModal.ts
@@ -2,15 +2,33 @@ export const settingsModal = () => {
 	return `
     <dialog id="settings-modal" class="rounded-3 border border-2 border-secondary">
             <div class="modal-header p-3 border-1 border-bottom d-flex justify-content-between align-items-center">
-                <h1 class="fs-6 fw-bold m-0 p-0">Settings</h1><i class="bi bi-x-lg fw-bold" type="button" id="settings-cancel" title="Close Modal"></i>
+                <h1 class="fs-4 fw-bold m-0 p-0">Settings</h1><i class="bi bi-x-lg fw-bold" type="button" id="settings-cancel" title="Close Modal"></i>
             </div>
-            <div class="modal-body p-3 d-flex justify-content-around">
-                settings go here
-            </div>
+            <form class="settings-form">
+                <div class="modal-body p-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="theme" id="default" value="default" title="Select Default Theme" checked>
+                        <label class="form-check-label" for="default">
+                            Default
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="theme" id="crimson" value="crimson" title="Select Crimson Theme">
+                        <label class="form-check-label" for="crimson">
+                            Crimson
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="theme" id="hot-pink" value="hot-pink" title="Select Hot Pink Theme">
+                        <label class="form-check-label" for="hot-pink">
+                            Hot Pink
+                        </label>
+                    </div>
+                </div>
+            </form>
             <div class="modal-footer p-3">                
                 <i class="bi bi-arrow-right-circle-fill fs-1" type="button" id="settings-confirm" title="Save Settings"></i>
-            </div>
-        
+            </div>        
     </dialog>
     `
 }
