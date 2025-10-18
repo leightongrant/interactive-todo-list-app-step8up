a0adff34ae536daa47876ebd7a0b1af0a77e7435
Leighton Grant <oneal.grant@outlook.com>
2025-10-18 21:34:44 +0100

    feat: Implement floating layout for desktop view
    This change introduces a fixed header and footer for screen sizes wider than 1200px to enhance user experience by keeping primary navigation and actions always accessible.
    
    - Sets `header` and `footer` to `position: fixed`.
    
    - Centers the fixed elements and constrains their width to align with the main application container.
    
    - Adjusts the `main` content area with a top margin to prevent overlap with the new fixed header.
    
    - Adds spacer elements to ensure a consistent background behind the floating header and footer.

--- src/components/layout.ts
+++ src/components/layout.ts
@@ -1,9 +1,11 @@
-import newTask from './newTask.js'
-import dateModal from './dateModal.js'
-import settingsModal from './settingsModal.js'
+import newTask from "./newTask.js";
+import dateModal from "./dateModal.js";
+import settingsModal from "./settingsModal.js";
 
 const layout = (header: string, footer: string): string => {
 	return `
+    <div class="fixed-top z-0 bg-white" style="height: 64px">
+    </div>
     <header class="sticky-top">
         <div class="container-fluid">
             <div class="header-content py-3 px-5">
@@ -11,7 +13,7 @@ const layout = (header: string, footer: string): string => {
             </div>
         </div>
     </header>
-    <main class="overflow-y-none">
+    <main class="overflow-y-none rounded-5">
         <div class="container-fluid d-grid content-wrapper">
             <div class="new-task-wrapper px-xl-5 py-4">
                 <div class="new-task d-flex rounded-4 pe-3 align-items-center">
@@ -32,8 +34,10 @@ const layout = (header: string, footer: string): string => {
             </div>
         </div>
     </footer>
+    <div class="fixed-bottom z-0 bg-white" style="height: 64px">
+    </div>
 
-`
-}
+`;
+};
 
-export default layout
+export default layout;
